21b4ecd363045290bfe160745bd7c67c91aac1c7
darkNoivern <crazycharizard23@gmail.com>
2022-09-05 11:34:10 +0530

    sign in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,118 +2,122 @@ import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import '../styles/navbar.css'
 import Rupee from '../images/rupee.png'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = (props) => {
-    
+
+    const navigate = useNavigate();
+
     const logout = () => {
         props.toggleUser(false)
+        navigate('/signin')
     }
 
-  return (
-    <>
-      
-        <nav className="navbar navbar-expand-md navbar-light">
-            <div className="container-fluid">
-                <Link exact to="/" className="navbar-brand mouse400 py-2">
-                    KharchaPani
-                    <img className='ms-2 navbar-logo' src={Rupee} alt="" />
-                </Link>
+    return (
+        <>
 
-                <button
-                    className="navbar-toggler voltorb-navbar-toggler bg-gate"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+            <nav className="navbar navbar-expand-md navbar-light">
+                <div className="container-fluid">
+                    <Link exact to="/" className="navbar-brand mouse400 py-2">
+                        KharchaPani
+                        <img className='ms-2 navbar-logo' src={Rupee} alt="" />
+                    </Link>
 
-                <div className="collapse navbar-collapse mt-2" id="navbarNav">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <button
+                        className="navbar-toggler voltorb-navbar-toggler bg-gate"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                        <li className="nav-item">
-                            <NavLink
-                                exact to={'/'}
-                                activeclassname="active"
-                                className="nav-link d-sm-none">
-                                <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
-                                    Home
-                                </span>
-                            </NavLink>
-                            <NavLink
-                                exact to={'/'}
-                                activeclassname="active"
-                                className="nav-link d-none d-sm-block">
-                                <span>
-                                    Home
-                                </span>
-                            </NavLink>
-                        </li>
+                    <div className="collapse navbar-collapse mt-2" id="navbarNav">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 
-                        <li className="nav-item">
-                            <NavLink
-                                exact to={'/transactions'}
-                                activeclassname="active"
-                                className="nav-link d-sm-none">
-                                <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
-                                    Transactions
-                                </span>
-                            </NavLink>
-                            <NavLink
-                                exact to={'/transactions'}
-                                activeclassname="active"
-                                className="nav-link d-none d-sm-block">
-                                <span>
-                                    Transactions
-                                </span>
-                            </NavLink>
-                        </li>
-
-                        <li className="nav-item">
-                            <NavLink
-                                exact to={'/add'}
-                                activeclassname="active"
-                                className="nav-link d-sm-none">
-                                <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
-                                    Add Transactions
-                                </span>
-                            </NavLink>
-                            <NavLink
-                                exact to={'/add'}
-                                activeclassname="active"
-                                className="nav-link d-none d-sm-block">
-                                <span>
-                                    Add Transactions
-                                </span>
-                            </NavLink>
-                        </li>
-
-                        <li className="nav-item">
-                            <NavLink
-                                exact to={'/manage'}
-                                activeclassname="active"
-                                className="nav-link d-sm-none">
-                                <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
-                                    Manage
-                                </span>
-                            </NavLink>
-                            <NavLink
-                                exact to={'/manage'}
-                                activeclassname="active"
-                                className="nav-link d-none d-sm-block">
-                                <span>
-                                    Manage
-                                </span>
-                            </NavLink>
-                        </li>
-
-                        {
-                            props.user === true
-                            ?
                             <li className="nav-item">
+                                <NavLink
+                                    exact to={'/'}
+                                    activeclassname="active"
+                                    className="nav-link d-sm-none">
+                                    <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
+                                        Home
+                                    </span>
+                                </NavLink>
+                                <NavLink
+                                    exact to={'/'}
+                                    activeclassname="active"
+                                    className="nav-link d-none d-sm-block">
+                                    <span>
+                                        Home
+                                    </span>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink
+                                    exact to={'/transactions'}
+                                    activeclassname="active"
+                                    className="nav-link d-sm-none">
+                                    <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
+                                        Transactions
+                                    </span>
+                                </NavLink>
+                                <NavLink
+                                    exact to={'/transactions'}
+                                    activeclassname="active"
+                                    className="nav-link d-none d-sm-block">
+                                    <span>
+                                        Transactions
+                                    </span>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink
+                                    exact to={'/add'}
+                                    activeclassname="active"
+                                    className="nav-link d-sm-none">
+                                    <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
+                                        Add Transactions
+                                    </span>
+                                </NavLink>
+                                <NavLink
+                                    exact to={'/add'}
+                                    activeclassname="active"
+                                    className="nav-link d-none d-sm-block">
+                                    <span>
+                                        Add Transactions
+                                    </span>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink
+                                    exact to={'/manage'}
+                                    activeclassname="active"
+                                    className="nav-link d-sm-none">
+                                    <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
+                                        Manage
+                                    </span>
+                                </NavLink>
+                                <NavLink
+                                    exact to={'/manage'}
+                                    activeclassname="active"
+                                    className="nav-link d-none d-sm-block">
+                                    <span>
+                                        Manage
+                                    </span>
+                                </NavLink>
+                            </li>
+
+                            {
+                                props.user === true
+                                    ?
+                                    <li className="nav-item">
                                         <NavLink
                                             onClick={() => (logout())}
                                             exact to={'/signin'}
@@ -134,34 +138,32 @@ const Navbar = (props) => {
                                         </NavLink>
                                     </li>
                                     :
+                                    <li className="nav-item">
+                                        <NavLink
+                                            exact to={'/signin'}
+                                            activeclassname="active"
+                                            className="nav-link d-sm-none">
+                                            <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
+                                                SignUp
+                                            </span>
+                                        </NavLink>
+                                        <NavLink
+                                            exact to={'/signin'}
+                                            activeclassname="active"
+                                            className="nav-link d-none d-sm-block">
+                                            <span>
+                                                SignUp
+                                            </span>
+                                        </NavLink>
+                                    </li>
+                            }
 
-
-                        <li className="nav-item">
-                            <NavLink
-                                exact to={'/signup'}
-                                activeclassname="active"
-                                className="nav-link d-sm-none">
-                                <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
-                                    SignUp
-                                </span>
-                            </NavLink>
-                            <NavLink
-                                exact to={'/signin'}
-                                activeclassname="active"
-                                className="nav-link d-none d-sm-block">
-                                <span>
-                                    SignUp
-                                </span>
-                            </NavLink>
-                        </li>
-                        }
-
-                    </ul>
+                        </ul>
                     </div>
-            </div>
-        </nav>
-    </>
-  )
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
